04251e0f17f697c47321b17dc6986d132dc9b225
evanmu <MuPeng@MP-PC>
2016-07-20 08:15:20 +0800

    kafka 0.8版本配置，可运行

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ nohup java \
 -server \
 -XX:+PrintGCDetails \
 -XX:+PrintGCTimeStamps \
--classpath config:target/dependencies/*:target/dcmonitor-0.1.4.jar \
-com.sf.monitor.DCMonitor > /data/logs/dcmonitor/dcmonitor.run.out 2>&1 &
+-classpath config:dependencies/*:dcmonitor-0.1.4.jar \
+com.sf.monitor.DCMonitor > logs/dcmonitor.run.out 2>&1 &
--- src/main/java/com/sf/monitor/Resources.java
+++ src/main/java/com/sf/monitor/Resources.java
@@ -21,72 +21,76 @@ import org.apache.curator.framework.imps.GzipCompressionProvider;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 public class Resources {
-  private static final Logger log = new Logger(Resources.class);
-  public static ThreadLocal<Boolean> isZKDirectBytes = new ThreadLocal<Boolean>() {
-    @Override
-    protected Boolean initialValue() {
-      return false;
+    private static final Logger log = new Logger(Resources.class);
+    public static ThreadLocal<Boolean> isZKDirectBytes = new ThreadLocal<Boolean>() {
+        @Override
+        protected Boolean initialValue() {
+            return false;
+        }
+    };
+
+    private static final ZkSerializer zkSerializer = new ZkSerializer() {
+        @Override
+        public byte[] serialize(Object data) throws ZkMarshallingError {
+            return ZKStringSerializer.serialize(data);
+        }
+
+        @Override
+        public Object deserialize(byte[] bytes) throws ZkMarshallingError {
+            if (isZKDirectBytes.get()) {
+                return bytes;
+            } else {
+                return ZKStringSerializer.deserialize(bytes);
+            }
+        }
+    };
+
+    public static CuratorFramework curator;
+    public static KafkaInfos kafkaInfos;
+    public static DruidInfos druidInfos;
+    public static ZookeeperHosts zkHosts;
+    public static Notify notify;
+    public static Config.Fetchers fetchers;
+    public static PrometheusQuery prometheusQuery;
+
+    public static final ObjectMapper jsonMapper = new ObjectMapper();
+
+    static {
+        jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jsonMapper.configure(SerializationFeature.INDENT_OUTPUT, false);
     }
-  };
 
-  private static final ZkSerializer zkSerializer = new ZkSerializer() {
-    @Override
-    public byte[] serialize(Object data) throws ZkMarshallingError {
-      return ZKStringSerializer.serialize(data);
+    public static void init() throws Exception {
+        try {
+            Config.CuratorConfig zkConfig = Config.config.zookeeper;
+            curator = CuratorFrameworkFactory
+                    .builder()
+                    .connectString(zkConfig.addrs)
+                    .retryPolicy(
+                            new ExponentialBackoffRetry(zkConfig.baseSleepTimeMs, zkConfig.maxRetries,
+                                    zkConfig.maxSleepMs)).compressionProvider(new GzipCompressionProvider()).build();
+            curator.start();
+            kafkaInfos = new KafkaInfos(new ZkClient(zkConfig.addrs, 30000, zkConfig.connectionTimeout, zkSerializer));
+
+            zkHosts = new ZookeeperHosts(Config.config.zookeeper.addrs);
+
+            druidInfos = Config.config.druidInfos;
+            if (null != druidInfos) {
+                druidInfos.init();
+            }
+//            notify = new Notify(Config.config.notify.url);
+            fetchers = Config.config.fetchers;
+
+            Prometheus.defaultInitialize();
+            prometheusQuery = new PrometheusQuery(Config.config.prometheus.serverUrl);
+        } catch (Exception e) {
+            log.error(e, "error while initialyzing resourses");
+            throw e;
+        }
     }
 
-    @Override
-    public Object deserialize(byte[] bytes) throws ZkMarshallingError {
-      if (isZKDirectBytes.get()) {
-        return bytes;
-      } else {
-        return ZKStringSerializer.deserialize(bytes);
-      }
+    public static void close() {
+        IOUtils.closeQuietly(kafkaInfos);
+        IOUtils.closeQuietly(curator);
     }
-  };
-
-  public static CuratorFramework curator;
-  public static KafkaInfos kafkaInfos;
-  public static DruidInfos druidInfos;
-  public static ZookeeperHosts zkHosts;
-  public static Notify notify;
-  public static Config.Fetchers fetchers;
-  public static PrometheusQuery prometheusQuery;
-
-  public static final ObjectMapper jsonMapper = new ObjectMapper();
-
-  static {
-    jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    jsonMapper.configure(SerializationFeature.INDENT_OUTPUT, false);
-  }
-
-  public static void init() throws Exception {
-    try {
-      Config.CuratorConfig zkConfig = Config.config.zookeeper;
-      curator = CuratorFrameworkFactory
-        .builder()
-        .connectString(zkConfig.addrs)
-        .retryPolicy(new ExponentialBackoffRetry(zkConfig.baseSleepTimeMs, zkConfig.maxRetries, zkConfig.maxSleepMs))
-        .compressionProvider(new GzipCompressionProvider())
-        .build();
-      curator.start();
-      kafkaInfos = new KafkaInfos(new ZkClient(zkConfig.addrs, 30000, zkConfig.connectionTimeout, zkSerializer));
-      druidInfos = Config.config.druidInfos;
-      zkHosts = new ZookeeperHosts(Config.config.zookeeper.addrs);
-      druidInfos.init();
-      notify = new Notify(Config.config.notify.url);
-      fetchers = Config.config.fetchers;
-
-      Prometheus.defaultInitialize();
-      prometheusQuery = new PrometheusQuery(Config.config.prometheus.serverUrl);
-    } catch (Exception e) {
-      log.error(e, "error while initialyzing resourses");
-      throw e;
-    }
-  }
-
-  public static void close() {
-    IOUtils.closeQuietly(kafkaInfos);
-    IOUtils.closeQuietly(curator);
-  }
 }
